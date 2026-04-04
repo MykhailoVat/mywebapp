771c8b8de3ff759e5f5edaf68a49e7fd4d7de3f7
mike <vatolkinmihajlo@gmail.com>
2026-04-04 18:49:07 +0300

    fix crucial bug in pool.js

--- src/repositories/pool.js
+++ src/repositories/pool.js
@@ -34,11 +34,11 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 const pool = new Pool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    host: host,
+    port: port,
+    user: user,
+    password: password,
+    database: name,
 });
 
 export default pool
