b8e1bdcef7ec6900535f20b387192e2aeddc974a
mike <vatolkinmihajlo@gmail.com>
2026-04-05 20:59:08 +0300

    remove unneccesery sudo in scripts

--- scripts/users.sh
+++ scripts/users.sh
@@ -17,21 +17,21 @@ STUDENT_PASS="student123"
 
 DEFAULT_USER=$(awk -F: '$3 >= 1000 {print $1}' /etc/passwd)
 
-sudo useradd -r -s /bin/bash $APP_USER
-sudo useradd -m -g $GROUP -s /bin/bash $OPERATOR
-sudo useradd -m -s /bin/bash $TEACHER
-sudo useradd -m -s /bin/bash $STUDENT
+useradd -r -s /bin/bash $APP_USER
+useradd -m -g $GROUP -s /bin/bash $OPERATOR
+useradd -m -s /bin/bash $TEACHER
+useradd -m -s /bin/bash $STUDENT
 
 echo "$OPERATOR:$OPERATOR_PASS" | sudo chpasswd
 echo "$TEACHER:$TEACHER_PASS" | sudo chpasswd
 echo "$STUDENT:$STUDENT_PASS" | sudo chpasswd
 
-sudo chage -d 0 $OPERATOR
-sudo chage -d 0 $TEACHER
+chage -d 0 $OPERATOR
+chage -d 0 $TEACHER
 
 SUDO_FILE="/etc/sudoers.d/operator"
 
-sudo bash -c "cat > $SUDO_FILE" <<EOF
+bash -c "cat > $SUDO_FILE" <<EOF
 $OPERATOR ALL=(ALL) NOPASSWD: /bin/systemctl start mywebapp.socket
 $OPERATOR ALL=(ALL) NOPASSWD: /bin/systemctl stop mywebapp.socket
 $OPERATOR ALL=(ALL) NOPASSWD: /bin/systemctl status mywebapp.socket
@@ -39,11 +39,11 @@ $OPERATOR ALL=(ALL) NOPASSWD: /bin/systemctl restart mywebapp.socket
 $OPERATOR ALL=(ALL) NOPASSWD: /bin/systemctl reload nginx
 EOF
 
-sudo chmod 440 $SUDO_FILE
+chmod 440 $SUDO_FILE
 
-sudo mkdir -p $APP_DIR
-sudo chown -R $APP_USER:$APP_USER $APP_DIR
-sudo chmod -R 750 $APP_DIR
+mkdir -p $APP_DIR
+chown -R $APP_USER:$APP_USER $APP_DIR
+chmod -R 750 $APP_DIR
 
 #sudo usermod -L $DEFAULT_USER
 
