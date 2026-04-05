241987ee1cd1ad298a7f54ec9b45ebfe538cc83f
mike <vatolkinmihajlo@gmail.com>
2026-04-06 01:40:59 +0300

    fix permissions on config.sh, now give permission on whole directory

--- scripts/config.sh
+++ scripts/config.sh
@@ -4,7 +4,7 @@ set -e
 mkdir /etc/mywebapp
 cp config.json /etc/mywebapp/
 
-chown root:app /etc/mywebapp/config.json
+chown root:app /etc/mywebapp/
 chmod 640 /etc/mywebapp/config.json
 chmod 750 /etc/mywebapp
 
