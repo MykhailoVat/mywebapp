f7d50805f238e4990d6e7832ba7105f97c6273c8
MykhailoVat <vatolkinmihajlo@gmail.com>
2026-05-27 01:44:44 +0300

    made nginx dynamic

--- bare_setup/nginx.sh
+++ bare_setup/nginx.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+envsubst < configs/mywebapp.template > configs/mywebapp
+
 cp configs/mywebapp /etc/nginx/sites-available/
 ln -sf /etc/nginx/sites-available/mywebapp /etc/nginx/sites-enabled/
 rm -f /etc/nginx/sites-enabled/default
