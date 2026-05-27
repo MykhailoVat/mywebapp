c28c6e31f31cd4b28e273d89b1d57f2c73b33566
MykhailoVat <vatolkinmihajlo@gmail.com>
2026-05-27 16:26:55 +0300

    add envburst for socket

--- setup/systemd.sh
+++ setup/systemd.sh
@@ -3,6 +3,8 @@ set -e
 
 mkdir /etc/systemd/system/mywebapp
 cp configs/mywebapp.service /etc/systemd/system/
+
+envsubst < configs/mywebapp.socket.template > configs/mywebapp.socket
 cp configs/mywebapp.socket /etc/systemd/system/
 
 systemctl daemon-reload
